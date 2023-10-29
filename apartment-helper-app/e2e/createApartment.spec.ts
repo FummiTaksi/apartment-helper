42eaa7ef31527c95ba69cc1a8424f7b325c7594d
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2023-10-29 19:45:10 +0200

    add blank lines

--- apartment-helper-app/e2e/createApartment.spec.ts
+++ apartment-helper-app/e2e/createApartment.spec.ts
@@ -5,13 +5,17 @@ test.describe('Create apartment', () => {
     await page.getByPlaceholder('Username').type('admin')
     await page.getByPlaceholder('Password').type('password')
     await page.getByRole('button').click()
+
     await page.getByTestId('create-new-apartment-link').click()
+
     await page.getByTestId('apartment-url').fill('www.example.com')
     await page.getByTestId('apartment-title').fill('Great apartment')
     await page
       .getByTestId('apartment-description')
       .fill('Has something special.')
+
     await page.getByTestId('submit-apartment').click()
+
     await expect(page.getByTestId('create-apartment-success')).toBeVisible()
   })
 })
