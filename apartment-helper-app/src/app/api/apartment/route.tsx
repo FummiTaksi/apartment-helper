1ebb8e219d0f3a019a63baa4b34a0c8ab44860ca
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2024-04-14 12:28:27 +0300

    decode jwt token from cookies, set 401 status code if user not autheneteicated

--- apartment-helper-app/src/app/api/apartment/route.tsx
+++ apartment-helper-app/src/app/api/apartment/route.tsx
@@ -1,8 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { PrismaClient } from '@/generated/prisma-client'
-import { getToken } from 'next-auth/jwt'
+import { decode } from 'next-auth/jwt'
+import { cookies } from 'next/headers'
 
 export async function POST(req: NextRequest) {
+  if (process.env.SECRET === undefined) {
+    throw new Error('SECRET env not set')
+  }
+  const sessionToken = cookies().get('next-auth.session-token')
+  const decoded = await decode({
+    token: sessionToken?.value,
+    secret: process.env.SECRET,
+  })
+
+  if (decoded === null) {
+    return NextResponse.json({ status: 401, message: 'Unauthorized' })
+  }
+
   const prisma = new PrismaClient()
 
   const apartment = await req.json()
@@ -15,6 +29,19 @@ export async function POST(req: NextRequest) {
 }
 
 export async function GET(req: NextRequest) {
+  if (process.env.SECRET === undefined) {
+    throw new Error('SECRET env not set')
+  }
+  const sessionToken = cookies().get('next-auth.session-token')
+  const decoded = await decode({
+    token: sessionToken?.value,
+    secret: process.env.SECRET,
+  })
+
+  if (decoded === null) {
+    return NextResponse.json({ status: 401, message: 'Unauthorized' })
+  }
+
   const prisma = new PrismaClient()
 
   const apartments = await prisma.apartment.findMany()
