12a03b88dc5d0c90861c5c3f403e713c3f7aa24a
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2024-04-14 12:52:31 +0300

    add error handling

--- apartment-helper-app/src/app/apartments/page.tsx
+++ apartment-helper-app/src/app/apartments/page.tsx
@@ -10,20 +10,31 @@ export default function Apartments() {
   const { data: session } = useSession()
 
   const [apartments, setApartments] = useState<Apartment[]>([])
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   useEffect(() => {
-    const fetchApartments = async (): Promise<void> => {
-      const response = await fetch('/api/apartment', {
-        method: 'GET',
-      })
-      const apartments = (await response.json()) as unknown as Apartment[]
-      setApartments(apartments)
+    try {
+      const fetchApartments = async (): Promise<void> => {
+        const response = await fetch('/api/apartment', {
+          method: 'GET',
+        })
+        if (response.status === 200) {
+          const apartments = (await response.json()) as unknown as Apartment[]
+          setApartments(apartments)
+          setErrorMessage(null)
+        }
+      }
+      fetchApartments()
+    } catch (e) {
+      setErrorMessage(`Something went wrong when fetching apartments, ${e}`)
     }
-    fetchApartments()
   }, [])
 
   if (session === null) {
     return null
+  }
+  if (errorMessage !== null) {
+    return <p>{errorMessage}</p>
   }
 
   return (
